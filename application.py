362f3c7a2be85293f53c6f75b85be392097d5abc
Alwin Tareen <altareen@gmail.com>
2021-03-16 16:46:04 -0400

    Added import statement

--- application.py
+++ application.py
@@ -53,7 +53,7 @@ if not os.environ.get("DATABASE_URL"):
 
 # Configure application to use PostgreSQL database
 #engine = create_engine(os.getenv("DATABASE_URL"))
-engine = create_engine("postgres://tdpmrxyzqtyhfx:6d929e192d129a30f2fe22aec6afcf7ee2846995a004b6b5d497ffd863b31065@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d79r0a8uiur6km")
+engine = create_engine("postgresql://tdpmrxyzqtyhfx:6d929e192d129a30f2fe22aec6afcf7ee2846995a004b6b5d497ffd863b31065@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d79r0a8uiur6km")
 db = scoped_session(sessionmaker(bind=engine))
 
 
